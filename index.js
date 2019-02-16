86670aa7322e032930d99342bf1fc451bf315833
Steve Winton <stevewinton@gmail.com>
2019-02-16 12:21:32 -0600

    Place app in repl context as read-only object
    And restore on clear

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
-const repl = require('repl');
+const Repl = require('repl');
 const App = require('@octokit/app');
 const { findPrivateKey } = require('./lib/private-key');
+const { extendWith } = require('./lib/utils');
 
-// Instantiate a new app and place it in global scope
+// Instantiate a new app
 const app = new App({ id: process.env.APP_ID, privateKey: findPrivateKey() });
-global.app = app;
 
-repl.start();
+// Context initializer
+const initializeContext = extendWith({ app });
+
+const repl = Repl.start();
+
+// Place app in repl context as a read-only (immutable) property
+initializeContext(repl.context);
+
+// Listen for the reset event
+repl.on('reset', initializeContext);
