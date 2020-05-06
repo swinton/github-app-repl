61d3d4cf8f3e44bccfc6f978ba9edb5037337d52
Steve Winton <stevewinton@gmail.com>
2020-05-06 13:17:57 -0500

    Update for Node 12

--- github-app-repl.js
+++ github-app-repl.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node --experimental-repl-await
-
 require('dotenv').config();
+
+const repl = require('repl');
 const path = require('path');
 const homedir = require('os').homedir();
-const Repl = require('repl');
+
 const App = require('./lib/app');
 const { findPrivateKey } = require('./lib/private-key');
 const { extendWith } = require('./lib/utils');
@@ -19,16 +20,14 @@ const { extendWith } = require('./lib/utils');
   // Context initializer
   const initializeContext = extendWith({ app, installations });
 
-  const repl = Repl.start();
+  const r = repl.start('> ');
 
   // Place app in repl context as a read-only (immutable) property
-  initializeContext(repl.context);
+  initializeContext(r.context);
 
   // Listen for the reset event
-  repl.on('reset', initializeContext);
+  r.on('reset', initializeContext);
 
   // Initialize a history log file
-  if (repl.setupHistory) {
-    repl.setupHistory(path.join(homedir, '.node_repl_history'));
-  }
+  r.setupHistory(path.join(homedir, '.node_repl_history'), () => {});
 })();
